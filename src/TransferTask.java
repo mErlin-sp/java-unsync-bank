e41f0edc95bca79d038a1e24c5ab979271e71ded
Oleksandr <merlin.ua.1@gmail.com>
2023-05-19 03:29:09 +0300

    Update using ForkJoinFramework

---
+++ src/TransferTask.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.RecursiveTask;
+
+public class TransferTask extends RecursiveTask<Long> {
+    private static final int THRESHOLD = 20;
+    private final Bank bank;
+    private final int fromAccount;
+    private final int maxAmount;
+    private final int reps;
+
+    public TransferTask(Bank b, int from, int max, int reps) {
+        bank = b;
+        fromAccount = from;
+        maxAmount = max;
+        this.reps = reps;
+    }
+
+    @Override
+    protected Long compute() {
+        if (reps > THRESHOLD) {
+            return ForkJoinTask.invokeAll(createSubtasks()).stream().mapToLong(ForkJoinTask::join).sum();
+        } else {
+            return processing(reps);
+        }
+    }
+
+    private Collection<TransferTask> createSubtasks() {
+        List<TransferTask> dividedTasks = new ArrayList<>();
+        dividedTasks.add(new TransferTask(bank, fromAccount, maxAmount, reps / 2));
+        dividedTasks.add(new TransferTask(bank, fromAccount, maxAmount, (reps - (reps / 2))));
+        return dividedTasks;
+    }
+
+    private Long processing(int reps) {
+        long sum = 0;
+        for (int i = 0; i < reps; i++) {
+            try {
+                int toAccount = (int) (bank.size() * Math.random());
+                int amount = (int) (maxAmount * Math.random() / reps);
+                bank.transfer(fromAccount, toAccount, amount);
+                sum += amount;
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return sum;
+    }
+}
--- src/UnSyncBankTest.java
+++ src/UnSyncBankTest.java
@@ -1,17 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.Future;
+
 /**
  * author Cay Horstmann
+ * modified by Oleksandr Popov
  */
+
 public class UnSyncBankTest {
-    public static final int NACCOUNTS = 10;
+    public static final int N_ACCOUNTS = 10;
     public static final int INITIAL_BALANCE = 10000;
+    public static final int REPS_FOR_ACCOUNT = 10000000;
 
     public static void main(String[] args) {
-        Bank b = new Bank(NACCOUNTS, INITIAL_BALANCE);
-        for (int i = 0; i < NACCOUNTS; i++) {
-            TransferThread t = new TransferThread(b, i,
-                    INITIAL_BALANCE);
-            t.setPriority(Thread.NORM_PRIORITY + i % 2);
-            t.start();
+        System.out.println("UnSync Bank Test with ForkJoinFramework");
+        Bank b = new Bank(N_ACCOUNTS, INITIAL_BALANCE);
+
+        long timer = System.currentTimeMillis();
+
+        try (ForkJoinPool commonPool = ForkJoinPool.commonPool()) {
+
+            List<Future<Long>> futures = new ArrayList<>();
+            for (int i = 0; i < N_ACCOUNTS; i++) {
+                futures.add(commonPool.submit(new TransferTask(b, i, INITIAL_BALANCE, REPS_FOR_ACCOUNT)));
+            }
+
+            for (int i = 0; i < futures.size(); i++) {
+                long result = futures.get(i).get();
+                System.out.println("Total transaction amount for account " + i + ": " + result);
+            }
+
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            System.out.println("Execution time: " + (System.currentTimeMillis() - timer));
+            System.out.println("Total amount in Bank: " + b.sum());
         }
     }
 }
